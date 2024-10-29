f70cfbf959aba410686049d6f376b7e1946c60a3
PiotrBlachlinski <116668375+PiotrBlachlinski@users.noreply.github.com>
2024-10-29 11:44:12 +0100

    Strony rejestracji i logowania

---
+++ Forum/frontend/src/Header.jsx
@@ -0,0 +1,44 @@
+import {Link} from "react-router-dom";
+export default function Header() {
+    return (
+        <header className='flex justify-between'>
+            {/* Przycisk przejścia na główną stronę*/}
+        <Link to={'/'} className='flex items-center gap-1'>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+        </svg>
+        <span className='font-bold text-l'>Forum</span>
+
+        </Link>
+
+        <div className='flex border border-gray-300 rounded-full p-3 gap-3'>
+          <div>Wyszukaj</div>
+          <div className='border-l border-gray-300'></div>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              </svg>
+            </button>
+        </div>
+
+        <div className='flex gam-2 border border-grey-300 rounded-full py-2 px-4 shadow-md shadow-gray-300 gap-2'>
+          <Link to={'/register'}>
+            {/* Przycisk rejestracji */}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+            </svg>
+          </Link>
+          {/* Przycisk logowania i profilu */} 
+          <div className='border-l border-gray-300'></div>
+          <Link to={'/login'}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+          </Link>
+        </div>
+
+      </header>
+    );
+
+}
---
+++ Forum/frontend/src/pages/IndexPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+import Header from "../Header";
+
+export default function IndexPage() {
+    return (
+    <div>
+      Tu w przyszłości będzie miejsce na posty
+    </div>
+    );
+}
