0485b26080501a60e3fdbbbef4036d281ef8b264
Błażej Klimas <115409422+VoidDemo@users.noreply.github.com>
2024-11-19 17:17:22 +0100

    Update WebConfig.java

--- Forum/backend/ForumBD/forum/src/main/java/com/bd/forum/config/WebConfig.java
+++ Forum/backend/ForumBD/forum/src/main/java/com/bd/forum/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.bd.forum.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,12 +13,12 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Umożliwia CORS dla wszystkich endpointów
-                        .allowedOrigins("http://localhost:5173") // Adres frontendu
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Dozwolone metody HTTP
-                        .allowedHeaders("*") // Dozwolone nagłówki
-                        .allowCredentials(true); // Umożliwia przesyłanie ciasteczek
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
